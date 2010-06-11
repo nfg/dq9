d4687f8fc9a021c8665b759ced0c19f6624bce47
nfg <nigelgregoire@gmail.com>
2010-06-11 12:50:02 -0700

    Adding shtuff to git.

---
+++ dq9.php
@@ -0,0 +1,17 @@
+<?php
+
+$days = ceil((strtotime("2010-07-11")-time())/24/60/60);
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head><title>DQ9 Countdown</title></head>
+<body>
+
+<div style="text-align: center">
+    <img src="dq9.png" alt="Dragon Quest IX: Sentinels of the Starry Skies" />
+    <h2><big><?php echo $days ?></big> days 'til the North American release.</h2>
+</div>
+
+</body>
+</html>
